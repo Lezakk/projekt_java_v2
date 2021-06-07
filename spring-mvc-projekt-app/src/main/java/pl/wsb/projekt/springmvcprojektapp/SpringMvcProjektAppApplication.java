bef2aef7367e246f59b44a7ec9fe0ccbeebcbdc1
Lezakk <wiktor.zak94@gmail.com>
2021-06-07 20:42:28 +0200

    new branch

---
+++ spring-mvc-projekt-app/src/main/java/pl/wsb/projekt/springmvcprojektapp/SpringMvcProjektAppApplication.java
@@ -0,0 +1,13 @@
+package pl.wsb.projekt.springmvcprojektapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProjektAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProjektAppApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-projekt-app/src/test/java/pl/wsb/projekt/springmvcprojektapp/SpringMvcProjektAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsb.projekt.springmvcprojektapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjektAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
